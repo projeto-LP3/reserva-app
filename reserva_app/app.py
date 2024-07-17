79b9df656523337cb5c023e876bd618bfa4b06a2
Estudante 1 <estudante1@debian.spo.ifsp.edu.br>
2024-07-17 17:08:16 -0300

    teste

---
+++ reserva_app/app.py
@@ -0,0 +1,7 @@
+    # Iniciar o projeto usando o código https://github.com/domingoslatorre/reserva-app
+    # Implementar os seguintes requisitos para todas as páginas:
+    # Rotas para todas as páginas
+    # Uso de templates e layouts
+    # Cadastrar sala e listar sala - arquivo csv
+    # Reservar sala - arquivo csv
+    # Cadastro usuário - arquivo csv
