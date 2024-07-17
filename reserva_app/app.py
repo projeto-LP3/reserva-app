c7677053249a4601f2f3483c01446a53e748f964
Estudante 1 <estudante1@debian.spo.ifsp.edu.br>
2024-07-17 17:27:19 -0300

    atualizações

--- reserva_app/app.py
+++ reserva_app/app.py
@@ -5,3 +5,30 @@
     # Cadastrar sala e listar sala - arquivo csv
     # Reservar sala - arquivo csv
     # Cadastro usuário - arquivo csv
+from flask import Flask, render_template
+
+app = Flask("Meu app")
+
+@app.route("/")
+def login():
+    return render_template("login.html")
+
+@app.route("/cadastrar-sala")
+def cadastrarsala():
+    return render_template("cadastrar-sala.html")
+
+@app.route("/cadastro")
+def cadastro():
+    return render_template("cadastro.html")
+
+@app.route("/listar-salas")
+def listarsalas():
+    return render_template("listar-salas.html")
+
+@app.route("/reservar-sala")
+def reservarsala():
+    return render_template("reservar-sala.html")
+
+@app.route("/reservas")
+def reservas():
+    return render_template("reservas.html")
