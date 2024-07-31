fda5373d2895391409d6030188c2ab9b8f144b9a
Estudante 1 <estudante1@debian.spo.ifsp.edu.br>
2024-07-31 15:58:03 -0300

    atualização

--- reserva_app/app.py
+++ reserva_app/app.py
@@ -1,13 +1,27 @@
 from flask import Flask, render_template, request, redirect, url_for
+
+from reserva_app.reserva import adicionar_reserva, adicionar_sala, adicionar_usuario, obter_salas
     
-lista_sala = [ 
-    {"tipo": "Sala de aula", "descricao": "PCs com intel i5, 8GB de RAM", "capacidade": "20", "ativa": True}
-]
+# lista_sala = [ 
+#     {"id": "1", "tipo": "Sala de aula", "descricao": "PCs com intel i5, 8GB de RAM", "capacidade": "20", "ativa": True}
+# ]
+
+# lista_usuario = [ 
+#     {"nome": "ana", "email": "maya@email.com", "senha": "sbhsgksg"}
+# ]
+
+# lista_reseva = [ 
+#     {"sala": "1", "inicio": "Sala de aula", "d": "PCs com intel i5, 8GB de RAM", "capacidade": "20", "ativa": True}
+# ]
 
 app = Flask("minha app")
 
 @app.route("/")
 def login():
+    return render_template("login.html")
+
+@app.route("/templates/login")
+def loginCadastro():
     return render_template("login.html")
 
 @app.route("/templates/cadastrar-sala")
@@ -18,28 +32,61 @@ def cadastrarsala():
 def cadastro():
     return render_template("cadastro.html")
 
-@app.route("/templates/listar-salas")
-def listarsalas():
-    return render_template("listar-salas.html", salas=lista_sala)
+@app.route("/templates/cadastro", methods=["POST"])
+def cadastro_us():
+    nome=request.form['nome']
+    email=request.form['email']
+    senha=request.form['senha']
+    usuario= {"nome": nome,"email": email, "senha": senha}
 
-@app.route("/listar-salas", methods=["POST"])
-def adicionar_sala():
+    adicionar_usuario(usuario)
+
+    return redirect(url_for("login"))
+
+@app.route("/templates/listar-salas")
+def listar_salas():
+    return render_template("listar-salas.html", salas=obter_salas())
+
+@app.route("/templates/cadastrar-sala", methods=["POST"])
+def adicionar_salaus():
+    id=1
     tipo=request.form['tipo']
     descricao=request.form['descricao']
     capacidade=request.form['capacidade']
-    ativa=request.form['ativa']
-    sala= {"tipo": tipo, "descricao": descricao, "capacidade": capacidade, "ativa": ativa}
-    lista_sala.append(sala)
+    ativa=True
+    sala= {"id": id,"tipo": tipo, "descricao": descricao, "capacidade": capacidade, "ativa": ativa}
 
-    return redirect(url_for("listar-salas"))
+    adicionar_sala(sala)
+
+
+    return redirect(url_for("listar_salas"))
 
 @app.route("/templates/reservar-sala")
 def reservarsala():
-    return render_template("reservar-sala.html")
+    return render_template("reservar-sala.html", salas=obter_salas())
+
+
 
 @app.route("/templates/reservas")
 def reservas():
     return render_template("reservas.html")
+
+@app.route("/templates/reservar-sala", methods=["POST"])
+def adicionar_reservaus():
+    sala=request.form['sala']
+    inicio=request.form['inicio']
+    fim=request.form['fim']
+    reserva= {"sala": sala,"inicio": inicio, "fim": fim}
+
+    adicionar_reserva(reserva)
+
+    
+    return redirect(url_for("adicionar_reservaus"))
+
+
+@app.route("/templates/reserva/detalhe-reserva")
+def detalhe_reserva():
+    return render_template("detalhe-reserva.html")
 
 
 app.run(port=5001)
--- reserva_app/reserva.py
+++ reserva_app/reserva.py
@@ -2,8 +2,9 @@ def obter_salas():
     with open("cadastro-sala.csv", "r") as file:
         lista_sala = []
         for linha in file:
-            tipo, descricao, capacidade, ativa = linha.strip().split(",")
+            id, tipo, descricao, capacidade, ativa = linha.strip().split(",")
             sala = {
+                "id": id,
                 "tipo": tipo,
                 "descricao": descricao,
                 "capacidade": capacidade,
@@ -12,19 +13,56 @@ def obter_salas():
             lista_sala.append(sala)
         
         return lista_sala
-
+    
 obter_salas()
 
 def adicionar_sala(s):
     with open("cadastro-sala.csv", "a") as file:
-        linha = f"\n{s['tipo']},{s['descricao']},{s['capacidade']},{s['ativa']}"
+        linha = f"{s['id']},{s['tipo']},{s['descricao']},{s['capacidade']},{s['ativa']}\n"
         file.write(linha)
 
-s1 = {
-    "tipo": "tipo",
-    "descricao": "descricao",
-    "capacidade": "capacidade",
-    "ativa": True   
-}
 
-adicionar_sala(s1)
+
+# adicionar_sala(s1)
+
+def obter_usuario():
+    with open("cadastro-usuario.csv", "r") as file:
+        lista_sala = []
+        for linha in file:
+            nome, email, senha= linha.strip().split(",")
+            usuario = {
+                "nome": nome,
+                "email": email,
+                "senha": senha
+            }
+            lista_sala.append(usuario)
+        
+        return lista_sala
+
+obter_usuario()
+
+def adicionar_usuario(u):
+    with open("cadastro-usuario.csv", "a") as file:
+        linha = f"{u['nome']},{u['email']},{u['senha']}\n"
+        file.write(linha)
+
+def obter_reserva():
+    with open("reserva-sala.csv", "r") as file:
+        lista_reserva = []
+        for linha in file:
+            sala, inicio, fim= linha.strip().split(",")
+            reserva = {
+                "sala": sala,
+                "inicio": inicio,
+                "fim": fim
+            }
+            lista_reserva.append(reserva)
+        
+        return lista_reserva
+
+obter_usuario()
+
+def adicionar_reserva(u):
+    with open("reserva-sala.csv", "a") as file:
+        linha = f"{u['sala']},{u['inicio']},{u['fim']}\n"
+        file.write(linha)
